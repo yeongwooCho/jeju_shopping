47c1e39a96ae004e2cf47a4fef6cfebc8dc334b9
yeongwoo <jyy0223@naver.com>
2024-03-13 22:58:04 +0900

    [add] color 와 text_style class static 변수로 선언

--- lib/common/const/colors.dart
+++ lib/common/const/colors.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
-// Main
-const PRIMARY_COLOR = Color(0xFFFF8254);
-const SECONDARY_COLOR = Color(0xFFCA3600);
+class MyColor {
+  // Main
+  static const primary = Color(0xFFFF8254);
+  static const secondary = Color(0xFFCA3600);
 
-// Text Color
-const TEXT_COLOR = Color(0xFF1C1C1E);
-const WHITE_COLOR = Color(0xFFFDFDFD);
-const ERROR_COLOR = Color(0xFFFF555F);
-const NOTICE_COLOR = Color(0xFFEBF4FF);
+  // Text Color
+  static const text = Color(0xFF1C1C1E);
+  static const white = Color(0xFFFDFDFD);
+  static const error = Color(0xFFFF555F);
+  static const notice = Color(0xFFEBF4FF);
 
-const DARK_GREY_COLOR = Color(0xFFEBF4FF);
-const MIDDLE_GREY_COLOR = Color(0xFFEBF4FF);
-const LIGHT_GREY_COLOR = Color(0xFFEBF4FF);
-const BARRIER_COLOR = Color(0x33EBF4FF);
+  static const darkGrey = Color(0xFFEBF4FF);
+  static const middleGrey = Color(0xFFEBF4FF);
+  static const lightGrey = Color(0xFFEBF4FF);
+  static const barrier = Color(0x33EBF4FF);
+}
---
+++ lib/common/const/text_styles.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class MyTextStyle {
+  static const letterSpacing = -1.025;
+  static const height = 1.2;
+
+  static const appName = TextStyle(
+    fontSize: 36.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const headTitle = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const bodyTitleBold = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const bodyBold = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const bodyMedium = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const bodyRegular = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const descriptionBold = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const descriptionMedium = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+
+  static const descriptionRegular = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+}
