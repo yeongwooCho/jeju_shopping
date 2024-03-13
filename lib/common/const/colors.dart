980c88a32c4fb267af59bc9f67d253ba0ada8132
yeongwoo <jyy0223@naver.com>
2024-03-13 18:06:59 +0900

    [add] color file

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Main
+const PRIMARY_COLOR = Color(0xFFFF8254);
+const SECONDARY_COLOR = Color(0xFFCA3600);
+
+// Text Color
+const TEXT_COLOR = Color(0xFF1C1C1E);
+const WHITE_COLOR = Color(0xFFFDFDFD);
+const ERROR_COLOR = Color(0xFFFF555F);
+const NOTICE_COLOR = Color(0xFFEBF4FF);
+
+const DARK_GREY_COLOR = Color(0xFFEBF4FF);
+const MIDDLE_GREY_COLOR = Color(0xFFEBF4FF);
+const LIGHT_GREY_COLOR = Color(0xFFEBF4FF);
+const BARRIER_COLOR = Color(0x33EBF4FF);
