6a0277d34c775b9043ace453e7d3b3399f2201df
yeongwoo <jyy0223@naver.com>
2024-03-16 18:35:39 +0900

    [add] flutter_locations 와 intl 을 이용한 현지화 시간 기능 셋팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jeju_shopping/common/const/colors.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:jeju_shopping/common/provider/go_router.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
   runApp(
     ProviderScope(
       observers: [],
@@ -22,6 +27,15 @@ class _App extends ConsumerWidget {
       theme: ThemeData(
         fontFamily: 'Pretendard',
       ),
+      localizationsDelegates: const [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+        Locale('ko', ''), // Korean, no country code
+      ],
       routerConfig: router,
     );
   }
