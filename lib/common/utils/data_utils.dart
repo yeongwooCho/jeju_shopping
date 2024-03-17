3490156b602012a4131f5ade35a10d46537bae9f
yeongwoo <jyy0223@naver.com>
2024-03-17 23:58:10 +0900

    [add] convertPriceToMoneyString 추가

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,7 +1,17 @@
+import 'package:intl/intl.dart';
+
 class DataUtils {
   static String convertDateTimeToDateString({
     required DateTime datetime,
   }) {
     return '${datetime.year}-${datetime.month.toString().padLeft(2, '0')}-${datetime.day.toString().padLeft(2, '0')}';
+  }
+
+  static String convertPriceToMoneyString({
+    required int price,
+  }) {
+    final NumberFormat numberFormat = NumberFormat('###,###,###,###');
+
+    return numberFormat.format(price);
   }
 }
