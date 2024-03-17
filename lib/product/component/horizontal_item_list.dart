3bbd4b9b53d7bee11f517b0e949e254d87a99976
yeongwoo <jyy0223@naver.com>
2024-03-18 00:00:55 +0900

    [add] home screen 화면 동작 구현

---
+++ lib/product/component/horizontal_item_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:jeju_shopping/common/const/text_styles.dart';
+import 'package:jeju_shopping/common/utils/data_utils.dart';
+import 'package:jeju_shopping/product/model/product_model.dart';
+
+class HorizontalItemList extends StatelessWidget {
+  final List<ProductModel> products;
+
+  const HorizontalItemList({
+    super.key,
+    required this.products,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 214.0,
+      child: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24.0,
+        ),
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+
+          return SizedBox(
+            width: 140.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                  height: 140.0,
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  product.name,
+                  style: MyTextStyle.descriptionRegular,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  DataUtils.convertPriceToMoneyString(price: product.price),
+                  style: MyTextStyle.bodyBold,
+                ),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(width: 16.0);
+        },
+      ),
+    );
+  }
+}
--- lib/product/view/product_screen.dart
+++ lib/product/view/product_screen.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jeju_shopping/common/component/divider_container.dart';
+import 'package:jeju_shopping/common/component/see_more_button.dart';
 import 'package:jeju_shopping/common/const/colors.dart';
 import 'package:jeju_shopping/common/const/text_styles.dart';
 import 'package:jeju_shopping/common/layout/default_app_bar.dart';
 import 'package:jeju_shopping/common/layout/default_layout.dart';
+import 'package:jeju_shopping/product/component/horizontal_item_list.dart';
+import 'package:jeju_shopping/product/model/product_model.dart';
+import 'package:jeju_shopping/product/provider/product_provider.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends ConsumerWidget {
   static String get routeName => "product";
 
   const ProductScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(productProvider);
+
     return DefaultLayout(
       appbar: const DefaultAppBar(title: '홈'),
       child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
         slivers: [
           SliverToBoxAdapter(
             child: Image.asset(
@@ -22,8 +31,44 @@ class ProductScreen extends StatelessWidget {
             ),
           ),
           renderCategories(),
-          SliverToBoxAdapter(
-              child: Container(height: 10.0, color: MyColor.lightGrey))
+          SliverToBoxAdapter(child: DividerContainer()),
+          SliverToBoxAdapter(child: const SizedBox(height: 40.0)),
+          renderProducts(title: '신선하고 깔끔한 프레시 상품', products: state),
+          SliverToBoxAdapter(child: const SizedBox(height: 40.0)),
+          renderProducts(title: '가장 많이 판매된 베스트 상품', products: state),
+          SliverToBoxAdapter(child: const SizedBox(height: 40.0)),
+          renderProducts(title: '고객님 맞춤 추천 상품', products: state),
+          SliverToBoxAdapter(child: const SizedBox(height: 40.0)),
+        ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderProducts({
+    required String title,
+    required List<ProductModel> products,
+  }) {
+    return SliverToBoxAdapter(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: MyTextStyle.bodyTitleBold,
+                ),
+                SeeMoreButton(
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          HorizontalItemList(products: products),
         ],
       ),
     );
@@ -87,17 +132,17 @@ class ProductScreen extends StatelessWidget {
   }) {
     switch (index) {
       case 0:
-        return '특산품';
+        return Category.special.label;
       case 1:
-        return '농산물';
+        return Category.agricultural.label;
       case 2:
-        return '수산물';
+        return Category.sea.label;
       case 3:
-        return '축산물';
+        return Category.livestock.label;
       case 4:
-        return '건강식';
+        return Category.healthy.label;
       case 5:
-        return '간편식';
+        return Category.convenience.label;
       default:
         return '';
     }
